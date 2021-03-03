b330486a98268594387af4704bce9c8be8ae2d9a
SerranoPe <peter102195@yahoo.com>
2021-03-03 11:57:26 -0500

    Updates

---
+++ precache-manifest.53d8b52dc038afa5623e11db997881ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff7ac5d097661cddc7e4831a6336e098",
+    "url": "/SerranoPe/ProjectThree.git/index.html"
+  },
+  {
+    "revision": "9aeda124af848af6b341",
+    "url": "/SerranoPe/ProjectThree.git/static/css/main.62a6fb85.chunk.css"
+  },
+  {
+    "revision": "ad8fe39587a0516a9ad9",
+    "url": "/SerranoPe/ProjectThree.git/static/js/2.62c3754b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/SerranoPe/ProjectThree.git/static/js/2.62c3754b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aeda124af848af6b341",
+    "url": "/SerranoPe/ProjectThree.git/static/js/main.1dcf7dcf.chunk.js"
+  },
+  {
+    "revision": "5c54255f21f969320ea5",
+    "url": "/SerranoPe/ProjectThree.git/static/js/runtime-main.d4205324.js"
+  },
+  {
+    "revision": "f3caac4554718c65adccb45a341eca30",
+    "url": "/SerranoPe/ProjectThree.git/static/media/Banner.f3caac45.png"
+  }
+]);
